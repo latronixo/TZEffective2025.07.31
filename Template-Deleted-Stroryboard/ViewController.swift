154e53c86ef1c03cc68eec7bafe862990e92e46c
Valentin <125733534+latronixo@users.noreply.github.com>
2025-07-31 21:54:59 +0300

    Initial commit

---
+++ Template-Deleted-Stroryboard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  23.Project-4-FlagsInTableView
+//
+//  Created by Валентин Картошкин on 30.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .yellow
+    }
+
+
+}
+
