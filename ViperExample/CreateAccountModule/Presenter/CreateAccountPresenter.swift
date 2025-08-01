521b6eb4ffac5a6310d5e9d702523c0f40361568
Valentin <pamerra@mail.ru>
2025-08-01 13:19:23 +0300

    Описал протоколы презентера для взаимодействия с роутером

--- ViperExample/CreateAccountModule/Presenter/CreateAccountPresenter.swift
+++ ViperExample/CreateAccountModule/Presenter/CreateAccountPresenter.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+protocol CreateAccountPresenterInput {
+    var output: CreateAccountPresenterOutput
+}
+
+protocol CreateAccountPresenterOutput {
+    
+}
+
+final class CreateAccountPresenter {
+    
+    
+}
