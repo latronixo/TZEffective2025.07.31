af64860a3f698d357442e82ba27b9c204eaa1ff0
Valentin <pamerra@mail.ru>
2025-08-01 13:31:46 +0300

    Описал протокол роутера для взаимодействия с презентером

--- ViperExample/CreateAccountModule/Router/СreateAccountRouter.swift
+++ ViperExample/CreateAccountModule/Router/СreateAccountRouter.swift
@@ -5,4 +5,13 @@
 //  Created by Валентин on 31.07.2025.
 //
 
-import Foundation
+import UIKit
+
+protocol CreateAccountRouterInput {
+    func openAuthScreen()
+}
+
+final class CreateAccountRouter: CreateAccountRouterInput {
+    
+    
+}
