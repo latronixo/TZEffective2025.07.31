15cdfab48bfd17c8e6569577920b74892c5691ef
Valentin <pamerra@mail.ru>
2025-08-01 17:36:53 +0300

    Router готов

--- ViperExample/CreateAccountModule/Router/СreateAccountRouter.swift
+++ ViperExample/CreateAccountModule/Router/СreateAccountRouter.swift
@@ -8,10 +8,15 @@
 import UIKit
 
 protocol CreateAccountRouterInput {
-    func openAuthScreen()
+    func openAuthScreen(withLogin login: String)
 }
 
 final class CreateAccountRouter: CreateAccountRouterInput {
+    weak var rootViewController: UIViewController?
     
-    
+    func openAuthScreen(withLogin login: String) {
+        let newVc = UIViewController(nibName: nil, bundle: nil)
+        newVc.view.backgroundColor = .green
+        rootViewController?.present(newVc, animated: true, completion: nil)
+    }
 }
