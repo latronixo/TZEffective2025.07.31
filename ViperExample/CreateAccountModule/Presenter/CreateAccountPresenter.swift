d63562b51d3a92893f79cb4643f288f2f7acad63
Valentin <pamerra@mail.ru>
2025-08-01 14:24:48 +0300

    CreateAccountPresenter готов

--- ViperExample/CreateAccountModule/Presenter/CreateAccountPresenter.swift
+++ ViperExample/CreateAccountModule/Presenter/CreateAccountPresenter.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 protocol CreateAccountPresenterInput {
-    var output: CreateAccountPresenterOutput
+    var output: CreateAccountPresenterOutput? { get set }
 }
 
 protocol CreateAccountPresenterOutput {
@@ -16,6 +16,33 @@ protocol CreateAccountPresenterOutput {
 }
 
 final class CreateAccountPresenter {
+    weak var output: CreateAccountPresenterOutput?
     
+    private let interactor: CreateAccountInteractorOutput
+    private let router: CreateAccountRouterInput
+    private let view: CreateAccountViewInput
     
+    init(output: CreateAccountPresenterOutput? = nil, interactor: CreateAccountInteractorOutput, router: CreateAccountRouterInput, view: CreateAccountViewInput) {
+        self.output = output
+        self.interactor = interactor
+        self.router = router
+        self.view = view
+    }
+    
+}
+
+extension CreateAccountPresenter: CreateAccountViewOutput {
+    func userSelectCreateAccount(withLogin login: String, password: String) {
+        interactor.createAccount(withLogin: login, password: password)
+    }
+}
+
+extension CreateAccountPresenter: CreateAccountInteractorOutput {
+    func didReceive(error: String) {
+        
+    }
+    
+    func didReceive(withLogin login: String) {
+        router.openAuthScreen(withLogin: login)
+    }
 }
